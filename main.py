c680fd1e50942bc5edf27ab1133438729482da21
So.undac.he <140877727+Soundache@users.noreply.github.com>
2024-06-25 11:10:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
+from PyQt5 import uic
+import sys
+
+app = QApplication(sys.argv)
+
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+        uic.loadUi('prototype1.ui', self)
+        self.show()
+
+
+window = UI()
+window.show()
+sys.exit(app.exec_())
 
