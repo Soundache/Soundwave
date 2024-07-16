50ec96baa5013d6023bfb6d93a9ad290e846d059
So.undac.he <140877727+Soundache@users.noreply.github.com>
2024-07-16 11:07:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
 from PyQt5 import uic
 import sys
+from mido import Message, MidiFile, MidiTrack
+import pygame as pyg
+
+mid = MidiFile()
+track = MidiTrack()
+mid.tracks.append(track)
+
+track.append(Message('program_change', program=12, time=0))
+track.append(Message('note_on', note=64, velocity=64, time=32))
+track.append(Message('note_off', note=64, velocity=127, time=32))
+track.append(Message('note_on', note=65, velocity=64, time=32))
+track.append(Message('note_off', note=65, velocity=127, time=32))
+track.append(Message('note_on', note=60, velocity=64, time=32))
+track.append(Message('note_off', note=60, velocity=127, time=32))
+
+mid.save('song.mid')
+pyg.mixer.init()
+pyg.mixer.music.load('song.mid')
+pyg.mixer.music.play()
 
 app = QApplication(sys.argv)
+
 
 class UI(QMainWindow):
     def __init__(self):
@@ -14,4 +34,3 @@ class UI(QMainWindow):
 window = UI()
 window.show()
 sys.exit(app.exec_())
-
