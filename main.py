48f0f9755437929e16d3f4ca760c648bb28f538f
So.undac.he <140877727+Soundache@users.noreply.github.com>
2024-07-18 11:51:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ from PyQt5 import uic
 import sys
 from mido import Message, MidiFile, MidiTrack
 import pygame as pyg
+from pygame import midi
 
-mid = MidiFile()
-track = MidiTrack()
-mid.tracks.append(track)
+# mid = MidiFile()
+# track = MidiTrack()
+# mid.tracks.append(track)
 
-track.append(Message('program_change', program=12, time=0))
-track.append(Message('note_on', note=64, velocity=64, time=32))
-track.append(Message('note_off', note=64, velocity=127, time=32))
-track.append(Message('note_on', note=65, velocity=64, time=32))
-track.append(Message('note_off', note=65, velocity=127, time=32))
-track.append(Message('note_on', note=60, velocity=64, time=32))
-track.append(Message('note_off', note=60, velocity=127, time=32))
+# track.append(Message('program_change', program=12, time=0))
+# track.append(Message('note_on', note=64, velocity=64, time=32))
+# track.append(Message('note_off', note=64, velocity=127, time=32))
+# mid.save('song.mid')
 
-mid.save('song.mid')
-pyg.mixer.init()
-pyg.mixer.music.load('song.mid')
+pyg.init()
+pyg.mixer.music.load('song1.mid')
 pyg.mixer.music.play()
+
+midi.init()
+out = midi.Output()
+out.set_instrument(40)
+out.note_on(21)
+out.note_off(21)
 
 app = QApplication(sys.argv)
 
