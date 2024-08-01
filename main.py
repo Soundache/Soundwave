b8835c4042f88d7ba42f623940534fc4273280a9
So.undac.he <140877727+Soundache@users.noreply.github.com>
2024-08-01 11:29:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
+from PyQt5.QtCore import Qt
 from PyQt5 import uic
 import sys
 from mido import Message, MidiFile, MidiTrack
-import pygame as pyg
 import time
 from pygame import midi
-
-# mid = MidiFile()
-# track = MidiTrack()
-# mid.tracks.append(track)
-
-# track.append(Message('program_change', program=12, time=0))
-# track.append(Message('note_on', note=64, velocity=64, time=32))
-# track.append(Message('note_off', note=64, velocity=127, time=32))
-# mid.save('song.mid')
-
-# pyg.init()
-# pyg.mixer.music.load('song1.mid')
-# pyg.mixer.music.play()
 
 midi.init()
 out = midi.Output(midi.get_default_output_id(), 0)
 
 
-def play(instrument=0, note=72, duration=1.3):
+def play(instrument=0, note=72, duration=0.2):
     out.set_instrument(instrument)
     out.note_on(note, 127)
     time.sleep(duration)
@@ -38,6 +25,25 @@ class UI(QMainWindow):
         super(UI, self).__init__()
         uic.loadUi('prototype1.ui', self)
         self.show()
+        self.instrument = 0
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_C:
+            play(self.instrument, 74)
+        if event.key() == Qt.Key.Key_D:
+            play(self.instrument, 76)
+        if event.key() == Qt.Key.Key_E:
+            play(self.instrument, 78)
+        if event.key() == Qt.Key.Key_F:
+            play(self.instrument, 79)
+        if event.key() == Qt.Key.Key_G:
+            play(self.instrument, 81)
+        if event.key() == Qt.Key.Key_A:
+            play(self.instrument, 83)
+        if event.key() == Qt.Key.Key_B:
+            play(self.instrument, 85)
+        # if event.key() == Qt.Key.Key_c1:
+        #     play(self.instrument, 86)
 
 
 window = UI()
